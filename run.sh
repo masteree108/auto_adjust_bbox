d4a5df88fa73a95ecfb06dc486080fd0f2ecca0e
ivan_yu <gun123kk@gmail.com>
2021-09-12 21:28:45 +0800

    rename file for mobileNetSSD

--- run.sh
+++
@@ -1,5 +0,0 @@
-#script example
-#python auto_adjust_bbox.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video ~/HD/hd/Drone_Source/045/Drone_045.mp4
-
-#python auto_adjust_bbox.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video ~/HD/hd/Drone_Source/045/Drone_045.mp4
-python auto_adjust_bbox.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video video/FarToClose3.mp4
