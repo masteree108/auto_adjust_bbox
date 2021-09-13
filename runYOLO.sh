c269ce8c21dc2223dae8686759021c380c872c97
DNS-0522 <xkoukix111@gmail.com>
2021-09-13 23:55:32 +0800

    add YOLO model link & gpu command

---
+++ runYOLO.sh
@@ -0,0 +1,9 @@
+#script example
+#python coding/auto_adjust_bbox_yolo.py --video video/race.mp4 --yolo model/yolov4-coco --use-gpu 1
+#python coding/auto_adjust_bbox_yolo.py --video video/race.mp4 --yolo model/yolov4-coco --use-gpu 0
+
+Download yolov4 weight file
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
+
+Download yolov4 cfg file
+https://github.com/AlexeyAB/darknet/tree/master/cfg
