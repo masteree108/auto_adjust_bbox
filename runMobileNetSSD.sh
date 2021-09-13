6deeb809429736accdf6944d660d764705caf448
DNS-0522 <xkoukix111@gmail.com>
2021-09-13 23:41:11 +0800

    add gpu command

--- runMobileNetSSD.sh
+++ runMobileNetSSD.sh
@@ -1,5 +1,5 @@
 #script example
-#python auto_adjust_bbox_MobleNetSSD.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video ~/HD/hd/Drone_Source/045/Drone_045.mp4
+#python auto_adjust_bbox_MobleNetSSD.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video ~/HD/hd/Drone_Source/045/Drone_045.mp4 --use-gpu 1
 
-#python auto_adjust_bbox_MobileNetSSD.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video ~/HD/hd/Drone_Source/045/Drone_045.mp4
-python auto_adjust_bbox_MobileNetSSD.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video video/FarToClose3.mp4
+#python auto_adjust_bbox_MobileNetSSD.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video ~/HD/hd/Drone_Source/045/Drone_045.mp4 --use-gpu 0
+python auto_adjust_bbox_MobileNetSSD.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video video/FarToClose3.mp4 --use-gpu 0
